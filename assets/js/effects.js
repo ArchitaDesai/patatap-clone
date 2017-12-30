1eba572c45a3eb6da5c76ad35e7e7a4cd9660ec6
Archita Desai <architad8@gmail.com>
2017-12-30 10:29:33 +0530

    Put paperscript into its new js file

---
+++ assets/js/effects.js
@@ -0,0 +1,48 @@
+// Array to keep track of circles
+var circles = [];
+
+// When key is pressed down
+function onKeyDown(event){
+	keyPressedCode = event.event.keyCode;
+	if(keyPressedCode>=97 && keyPressedCode<=122)
+		generateCirclesAndSound(keyPressedCode-96);
+	else if(keyPressedCode>=65 && keyPressedCode<=90)
+		generateCirclesAndSound(keyPressedCode-64);
+}
+
+function generateCirclesAndSound(n){
+	// maxPoint will have size of the canvas, will be end point
+	var maxPoint = new Point(view.size.width, view.size.height);
+	// Point.random() generates point's X-Y values between 0 and 1
+	var randomPoint = Point.random()*maxPoint;
+	var newCircle = new Path.Circle(randomPoint,350);
+	newCircle.fillColor = generateRandomColor();
+	circles.push(newCircle);
+	
+	/* howlerjs - play sound */
+	new Howl({
+		src: ['assets/sounds/' + n + '.mp3']
+	}).play();
+}
+
+function generateRandomColor(){
+	return 'rgb(' + Math.random()*256 + ',' + Math.random()*256 + ',' + Math.random()*256 + ')';
+}
+
+// Animation handler, to animate all circles
+function onFrame(event){
+	for (var i = 0; i < circles.length; i++) {
+		// To animate its color
+		circles[i].fillColor.hue += 1;
+		// Circle's size will scale to 0.9 its original size
+		// Hence, will decrement and fade eventually
+		circles[i].scale(0.9);
+
+		//remove the circle from array if it's faded
+		if(circles[i].area < 1){
+			circles[i].remove();
+			circles.splice(i,1);
+			console.log(circles);
+		}
+	}
+}
